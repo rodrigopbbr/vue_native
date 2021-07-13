7f0c6288319e8c63437038c9e40c0a37fb3bfbe6
Rodrigo Pereira <rodrigopbbr@gmail.com>
2021-07-12 21:30:32 -0300

    Primeira Versão do App

---
+++ src/screens/tabs/components/tabFour.vue
@@ -0,0 +1,33 @@
+<template>
+  <nb-content padder :style="{ marginTop: 0 }">
+    <nb-card :style="{ flex: 0 }">
+      <nb-card-item>
+        <nb-body>
+          <nb-text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequuntur sunt itaque adipisci quisquam pariatur qui, reiciendis
+            architecto quod sint incidunt labore nisi totam illum numquam non
+            magnam praesentium, maxime quaerat!
+          </nb-text>
+        </nb-body>
+      </nb-card-item>
+    </nb-card>
+
+    <nb-card :style="{ flex: 1 }">
+      <nb-card-item>
+        <nb-body>
+          <nb-text>
+            NativeBase gives you the potential of building applications that run
+            on iOS and Android using a single codebase.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis totam aspernatur delectus perferendis, aut libero deserunt temporibus ipsum sit distinctio! Consectetur vero consequatur exercitationem distinctio veritatis eaque, aperiam commodi est.
+            Ipsum minima, obcaecati eos laboriosam nulla omnis consequatur vel asperiores quo officiis autem id cum, iusto dolores consectetur nisi tempora ad eveniet libero voluptatibus? Distinctio porro natus in ullam quae.
+            Aut consequatur nulla eligendi rerum quasi itaque harum, possimus quis fugiat nesciunt nostrum dolorem labore, asperiores, consectetur a. Inventore tenetur, similique ab quo labore voluptatum officia! Blanditiis doloremque aperiam nisi.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis totam aspernatur delectus perferendis, aut libero deserunt temporibus ipsum sit distinctio! Consectetur vero consequatur exercitationem distinctio veritatis eaque, aperiam commodi est.
+            Ipsum minima, obcaecati eos laboriosam nulla omnis consequatur vel asperiores quo officiis autem id cum, iusto dolores consectetur nisi tempora ad eveniet libero voluptatibus? Distinctio porro natus in ullam quae.
+            Aut consequatur nulla eligendi rerum quasi itaque harum, possimus quis fugiat nesciunt nostrum dolorem labore, asperiores, consectetur a. Inventore tenetur, similique ab quo labore voluptatum officia! Blanditiis doloremque aperiam nisi.            
+          </nb-text>
+        </nb-body>
+      </nb-card-item>
+    </nb-card>
+  </nb-content>
+</template>
